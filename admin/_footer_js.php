d5f5584c5cd028f7b4ca044bfdd685a697c96f5d
fdoromo <fdoromo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-27 14:10:17 +0000

    tags/2.2.350
    git-svn-id: https://plugins.svn.wordpress.org/total-security/tags/2.2.350@645025 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/_footer_js.php
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="<?php echo admin_url(); ?>load-scripts.php?c=0&amp;load=jquery-ui-core,jquery-ui-widget,jquery-ui-mouse,jquery-ui-sortable,postbox,post"></script>
+
