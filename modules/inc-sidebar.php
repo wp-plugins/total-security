00d36865493786d4fa36dc9df170b01c3e2f5f9d
fdoromo <fdoromo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-24 14:40:13 +0000

    lang
    git-svn-id: https://plugins.svn.wordpress.org/total-security/trunk@1144849 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/inc-sidebar.php
+++ modules/inc-sidebar.php
@@ -2,7 +2,7 @@
 # Donation Message #
 if( get_site_option( 'fdx1_hidden_time' ) && get_site_option( 'fdx1_hidden_time' ) < time() ) {
 echo '<div class="updated"><form method="post" action=""><input type="hidden" name="fdx_page" value="hide_message" /><p>';
-echo '<strong>'.__('Do you like this Plugin? Rate', $this->plugin_slug).' <span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span> '.__('on', $this->plugin_slug).' WordPress.org</strong> &nbsp;&nbsp;<input type="button" class="button button-primary" onClick="location.href=\''. $this->sbar_wpratelink . '\'" value="' . __( 'Vote & Rate Now', $this->plugin_slug ) . '">&nbsp;&nbsp;&nbsp;';
+echo '<strong>'.__('Do you like this Plugin? Rate', $this->hook).' <span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span> '.__('on', $this->hook).' WordPress.org</strong> &nbsp;&nbsp;<input type="button" class="button button-primary" onClick="location.href=\''. $this->sbar_wpratelink . '\'" value="' . __( 'Vote & Rate Now', $this->hook ) . '">&nbsp;&nbsp;&nbsp;';
 submit_button( __('Hide this message', $this->hook ), 'secondary', 'Submit', false, array( 'id' => '' ) ) ;
 echo '</p></form></div>';
 }
@@ -23,17 +23,6 @@ echo '<hr><div style="text-align: center; margin-top: 13px"><a href="http://api.
 echo'<a href="http://api.addthis.com/oexchange/0.8/forward/google_plusone_share/offer?url='. $this->sbar_homepage .'&amp;pubid=ra-52eb02b34be83059" data-width="500" data-height="600" rel="1" id="pop_2" class="newWindow" title="'. __( 'Share on', $this->hook ) .' Google Plus"><img src="'. plugins_url( 'images/h3_icons/googleplus.png', dirname(__FILE__)).'" width="32" height="32" border="0" alt="*" style="margin-right: 15px" /></a>';
 echo '<a href="http://api.addthis.com/oexchange/0.8/forward/twitter/offer?title=Plugin '. $this->pluginname . '&amp;url='. $this->sbar_homepage .'&amp;pubid=ra-52eb02b34be83059" data-width="500" data-height="690" rel="1" id="pop_5" class="newWindow" title="'. __( 'Share on', $this->hook ) .' Twitter"><img src="'. plugins_url( 'images/h3_icons/twitter.png', dirname(__FILE__)).'" width="32" height="32" border="0" alt="*" style="margin-right: 15px" /></a>';
 echo '<a href="http://api.addthis.com/oexchange/0.8/offer?title='. $this->pluginname . '&amp;url='. $this->sbar_homepage .'&amp;pubid=ra-52eb02b34be83059" data-width="500" data-height="690" rel="1" id="pop_6" class="newWindow" title="'. __( 'Share on', $this->hook ) .' Addthis"><img src="'. plugins_url( 'images/h3_icons/addthis.png', dirname(__FILE__)).'" width="32" height="32" border="0" alt="*" /></a></div>';
-echo '</div></div>';
-
-//----------------------------------------
-echo '<div class="postbox"><div class="handlediv" title="'.__('Click to toggle', $this->hook) .'"><br /></div><h3 class="hndle"><span>'. __( 'Translation', $this->hook ) . '</span></h3>';
-echo '<div class="inside">';
-if (WPLANG == '' || WPLANG == 'en' || WPLANG == 'en_US'  ){
-echo '<strong>Would you like to help translating this plugin?</strong><br/> Contribute a translation using the GlotPress web interface - no technical knowledge required (<strong><a href="' . $this->sbar_glotpress . '" target="_blank">how to</a></strong>)';
-} else {
-echo '<p><div class="dashicons dashicons-admin-users"></div>' .sprintf(__('Translated by: <a href="%s">YOUR NAME</a>', $this->hook), __('http://YOUR-LINK.COM', $this->hook) ) . '</p>';
-echo '<p>' . __( 'If you find any spelling error in this translation or would like to contribute', $this->hook ) . ', <a href="' . $this->sbar_glotpress . '" target="_blank">' . __( 'click here', $this->hook ) . '.</a></p>';
-}
 echo '</div></div>';
 
 //----------------------------------------
